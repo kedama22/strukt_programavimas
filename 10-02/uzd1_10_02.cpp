c0a2c1faaee5c271521edf73470039dc05976904
Kedama22 <s.gradeckaitt@gmail.com>
2025-10-08 09:11:49 +0300

    10-08 praktines paskaitos uzduotys (pabaigti)

---
+++ 10-02/uzd1_10_02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int masyvas[4];
+    int n=5, suma=0;
+    int didz;
+    cout<<"Iveskite masyvo elementa:"<<endl;
+    for (int i=0;i<n;i++){
+        cin>>masyvas[i];
+        suma+=masyvas[i];
+    }
+    // for (int i=0;i<n;i++){
+    //     suma+=masyvas[i];
+    // }
+    for (int i=0;i<n;i++) { //ISSIAISKINTI
+        didz=masyvas[i];
+        if(masyvas[i]>didz){
+            didz=masyvas[i];
+        }
+    }
+    cout<<suma<<didz<<endl;
+    return 0;
+}
---
+++ 10-08/uzd1_10_02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    int studentai[n][m];
+    cout<<"Iveskite studentu sk.: ";
+    cin>>n;
+    cout<<"Iveskite dalyku sk.: ";
+    cin>>m;
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<"Iveskite pazymius: ";
+            cin>>studentai[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        int j=0;
+        
+        }
+}
