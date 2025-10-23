955baafa54473d6e1ab66ec4a4e771714a67075c
Saule <s.gradeckaitt@gmail.com>
2025-10-24 00:37:12 +0300

    case 1 baigtas

--- praktines_uzd/3_praktine_uzd/3_praktine_uzd.cpp
+++ praktines_uzd/3_praktine_uzd/3_praktine_uzd.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-const char FIN1[]="bilietu_pardavimu_info.csv";
+const char FIN1[]="bilietu_pardavimu_info.txt";
 const char FIN2[]="salary.txt";
 const char FOUT2[]="new_salary.txt";
 // const char FOUT[]=""
 struct Zmogus {
     string pavarde;
     string vardas;
-    int esamas_atlyg;
-    int padidejimo_proc;
+    double esamas_atlyg;
+    double padidejimo_proc;
     double naujas_atlyg;
 };
 
@@ -28,27 +28,24 @@ void menu() {
 
 int main() {
     // int i=0,j=0;
-    double bilietai[10][10];
+    int bilietai[10][10];
     Zmogus darbuotojas[10];
     int pas;
     string eil;
-    double pard_bilietu_sk,bendra_pard_suma=0,pard_suma=0;
+    double pard_bilietu_sk=0,bendra_pard_suma=0,pard_suma=0;
     menu();
     cin>>pas;
-    do {
+    if(pas==1 || pas==2 || pas==3) {
         if (pas==3) {
             cout<<"Iseinama is programos"<<endl;
-            break;
-        }
-        if (pas!=1 && pas!=2) { //&& - IR
-            cout<<"Ivestas neteisingas pasirinkimas"<<endl;
-            break;
+            return 0;
         }
         switch (pas) {
             case 1:
             {
-                int i=0,j=0;
+                int j=0;
                 int n=0;
+                char a;
                 ifstream fin1(FIN1);
                 if (fin1.fail()) {
                     cout<<"Nepavyko atidaryti failo."<<endl;
@@ -57,35 +54,32 @@ int main() {
                 while(getline(fin1,eil)) { //skaiciuoja, kiek eiluciu yra fin1
                     n++;
                 }
-                cout<<n<<endl;
-                fin1.clear(); //istrina getline, kad galetu is naujo eiti per faila
-                for(int k=0;k<n;k++) {
-                    fin1>>bilietai[i][j];
-                    j++;
-                    fin1>>bilietai[i][j];
-                    i++;
+                fin1.clear();
+                fin1.seekg(0);
+                for(int i=0;i<n;i++) {
+                    fin1>>bilietai[i][j]>>a>>bilietai[i][j+1];
                     j=0;
                 }
-                i=0; //kad is naujo eitu per eilutes (PAZIURETI, AR VEIKIA)
-                for(int k=0;k<n-1;k++) {
-                    pard_bilietu_sk=bilietai[i][1]+bilietai[i+1][1];
+                int i=0;
+                for(int k=0;k<n;k++) {
+                    pard_bilietu_sk+=bilietai[i][1];
                     i++;
                 }
-                i=0;
-                for(int k=0;k<n-1;k++) {
+                for(int i=0;i<n;i++) {
                     pard_suma=bilietai[i][0]*bilietai[i][1];
                     bendra_pard_suma+=pard_suma;
                 }
                 fin1.close();
                 // ofstream fout (COUT??? kaip isvesti ats)
-                cout<<"Parduotu bilietu sk.: "<<pard_bilietu_sk<<endl;
-                cout<<"Bendra pardavimu suma: "<<bendra_pard_suma<<endl;
+                cout<<"Parduotu bilietu sk.: "<<fixed<<setprecision(2)<<pard_bilietu_sk<<endl;
+                cout<<"Bendra pardavimu suma: "<<fixed<<setprecision(2)<<bendra_pard_suma<<endl;
             }
             break;
             case 2:
             {
                 int i=0,j=0;
                 int n=0;
+                char a;
                 ifstream fin2(FIN2);
                 if (fin2.fail ()) {
                     cout<<"Nepavyko atidaryti failo."<<endl;
@@ -95,13 +89,12 @@ int main() {
                     n++;
                 }
                 fin2.clear();
-                fin2.seekg(0); //pirma eilute yra tekstas, todel reikia ja praleisti PASIDOMETI!!!
-                while(!fin2.eof()) {
+                //fin2.seekg(0); //pirma eilute yra tekstas, todel reikia ja praleisti PASIDOMETI!!!
+                for(i=0;i<n-1;i++) {
                     getline(fin2,darbuotojas[i].pavarde,',');
                     getline(fin2,darbuotojas[i].vardas,',');
-                    fin2>>darbuotojas[i].esamas_atlyg;
-                    fin2>>darbuotojas[i].padidejimo_proc;
-                    i++;
+                    fin2>>darbuotojas[i].esamas_atlyg>>a>>darbuotojas[i].padidejimo_proc;
+                    // fin2.ignore(1,'\n');
                 }
                 i=0;
                 ofstream fout2(FOUT2);
@@ -109,17 +102,21 @@ int main() {
                     cout<<"Nepavyko sukurti failo."<<endl;
                     break;
                 }
-                for(int k=0;k<n-1;k++) {
+                for(i=0;i<n-1;i++) {
                     darbuotojas[i].naujas_atlyg=darbuotojas[i].esamas_atlyg+(darbuotojas[i].esamas_atlyg*(darbuotojas[i].padidejimo_proc/100));
                 }
-                for(int k=0;k<n-1;k++) {
+                i=0;
+                for(i=0;i<n;i++) {
                     fout2<<darbuotojas[i].pavarde<<" "<<darbuotojas[i].vardas<<" "<<fixed<<setprecision(2)<<darbuotojas[i].naujas_atlyg<<endl;
                 }
+                cout<<"Nauji atlyginimai issaugoti faile: "<<FOUT2<<endl;
                 fin2.close();
                 fout2.close();
+                break;
             }
             break;
         }
-    } while (pas==1 || pas==2);
+    }
+    else cout<<"Neteisingas pasirinkimas"<<endl;
     return 0;
 }
