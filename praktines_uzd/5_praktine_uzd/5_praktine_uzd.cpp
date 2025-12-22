f5c079c16b0dfcc1ab5b21fc727535a213bd029e
Saule <s.gradeckaitt@gmail.com>
2025-12-22 13:16:38 +0200

    .

---
+++ praktines_uzd/5_praktine_uzd/5_praktine_uzd.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const FIN[]="testData.txt";
+const FOUT[]="results.txt";
+
+void klasesStatistika {
+
+}
+
+void studentoPaieskaID {
+
+}
+
+void OutputFailas {
+
+}
+
+void klausimuStatistika {
+
+}
+
+void sunkiausiasKlausimas {
+
+}
+
+int main() {
+
+    return 0;
+}
