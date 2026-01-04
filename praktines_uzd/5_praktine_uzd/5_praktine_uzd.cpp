f578c15a91fe9a9f42b5d9cd0b81caa11d7e4e6a
Saule <s.gradeckaitt@gmail.com>
2026-01-04 15:17:18 +0200

    baigta yippeeeeee

--- praktines_uzd/5_praktine_uzd/5_praktine_uzd.cpp
+++ praktines_uzd/5_praktine_uzd/5_praktine_uzd.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <sstream>
-#include <algorithm>
+#include <sstream>  //stringstream
+#include <algorithm> //find_if
 #include <iomanip>
 
 using namespace std;
@@ -13,6 +13,7 @@ const char FOUT[]="results.txt";
 struct Student {
     string studentID;
     string studentAtsakymai;
+    int studentTaskai;
     double studentGrade;
     double studentGradeProcent;
 };
@@ -29,28 +30,24 @@ void inputFailas() {
         return;
     }
     getline(fin,failo_ats);
-    stringstream failo_ats1(failo_ats);
+    stringstream failo_ats1(failo_ats); // atsakymu eilute pavercia i 20 skirtingu ats (T/F)
     char ats;
-    while (failo_ats1>>ats) {
+    while (failo_ats1>>ats) { //kol is pirmos fin eilute skaito po viena atsakymo char
         if (ats=='T' || ats=='F') { //stringstream praleidzia ','
-            teisingi_ats.push_back(ats);
+            teisingi_ats.push_back(ats); //ideda atsakymo char i vektoriaus 'teisingi_ats' gala
         }
     }
     while (getline(fin,eilute)) {
-        stringstream studento_info(eilute);
+        stringstream studento_info(eilute); //studento info pakeicia i du skirtingus info tipus
         Student temp;
         char temp_char;    
         studento_info>>temp.studentID; //sustos prie tarpo
-        while (studento_info>>temp_char) {
+        while (studento_info>>temp_char) { 
             if (temp_char=='T' || temp_char=='F' || temp_char=='-') {
                 temp.studentAtsakymai+=temp_char;
             }
-            // else {
-            //     cout<<"Faile ivestas neteisingas atsakymas"<<endl;
-            //     break;
-            // }
         }
-        studentai.push_back(temp);
+        studentai.push_back(temp); // viskas suskaiciuojama Student temp ir tik tada idedama i vektoriu
     }
     fin.close();
 }
@@ -71,7 +68,7 @@ void studentoPazymys() {
         double pazymioProcentai=0;
         int pazymioTaskai=0;
         int pazymys;
-        for (auto j=0; j<i->studentAtsakymai.length(); j++) {
+        for (auto j=0; j<i->studentAtsakymai.length(); j++) { // i-> - rodykle i studentai vektoriu
             if (i->studentAtsakymai[j] == teisingi_ats[j]) {
                 pazymioTaskai+=2;
             }
@@ -82,6 +79,9 @@ void studentoPazymys() {
                 continue;
             }
         }
+        i->studentTaskai=pazymioTaskai;
+        pazymioProcentai=(static_cast<double>(pazymioTaskai)/40)*100;
+        i->studentGradeProcent=pazymioProcentai;
         if (pazymioProcentai>=95 && pazymioProcentai<=100) pazymys=10;
         else if (pazymioProcentai>=85) pazymys=9;
         else if (pazymioProcentai>=75) pazymys=8;
@@ -93,13 +93,10 @@ void studentoPazymys() {
         else if (pazymioProcentai>=15) pazymys=2;
         else if (pazymioProcentai>=0 && pazymioProcentai<=14) pazymys=1;
         i->studentGrade=pazymys;        
-        pazymioProcentai=(static_cast<double>(pazymioTaskai)/40)*100;
-        i->studentGradeProcent=pazymioProcentai;
-
     }
 }
 
-void studentuPazymiuVidurkiai(double &vidurkisProcentu, double&vidurkisPazymiu) {
+void studentuPazymiuVidurkiai(double &vidurkisProcentu, double &vidurkisPazymiu) { //& pakeicia eil. 112-113 info
     double pazymioProcentai_vidurkis=0;
     double pazymioVidurkis=0;
     for (auto i=studentai.begin(); i!=studentai.end(); i++) {
@@ -110,6 +107,7 @@ void studentuPazymiuVidurkiai(double &vidurkisProcentu, double&vidurkisPazymiu) 
     vidurkisPazymiu=pazymioVidurkis/studentai.size();
 }
 
+// ----------- CASE 1 ------------
 void klasesStatistika() {
     double pazymioProcentai_vidurkis = 0;
     double pazymioVidurkis = 0;
@@ -117,6 +115,7 @@ void klasesStatistika() {
     studentoPazymys();
 // 1. STUDENTU SK
     cout<<"Studentu sk.: "<<studentai.size()<<endl;
+    cout<<"---------------------------------------"<<endl;
 
 // 2. DIDZIAUSIAS/MAZIAUSIAS PAZYMYS
     int didziausiasPazymys=0;
@@ -127,16 +126,18 @@ void klasesStatistika() {
         }
     }
     for (auto i=studentai.begin(); i!=studentai.end(); i++) {
-        if (i->studentGrade<maziausiasPazymys) {
+        if (i->studentGrade<=maziausiasPazymys) {
             maziausiasPazymys= i->studentGrade;
         }
     }
-    cout<<"Didziausias pazymys: "<<didziausiasPazymys<<"\n"<<"Maziausias pazymys: "<<maziausiasPazymys<<endl; //ISTRINTI
+    cout<<"Didziausias pazymys: "<<didziausiasPazymys<<"\n"<<"Maziausias pazymys: "<<maziausiasPazymys<<endl;
+    cout<<"---------------------------------------"<<endl;
 
 // 3. VIDUTINIS PAZYMYS/PROCENTAS
     studentuPazymiuVidurkiai(pazymioProcentai_vidurkis, pazymioVidurkis);
     cout<<"Vidutinis pazymys: "<<fixed<<setprecision(1)<<pazymioVidurkis<<endl;
     cout<<"Vidutinis procentas: "<<fixed<<setprecision(1)<<pazymioProcentai_vidurkis<<endl;
+    cout<<"---------------------------------------"<<endl;
 
 // 4. PAZYMIU PASISKIRSTYMAS
     int pazymiuPaskirstymas[11]={0};
@@ -156,18 +157,22 @@ void klasesStatistika() {
             cout<<i<<" balu: "<<pazymiuPaskirstymas[i]<<" mokiniu"<<endl;
         }
     }
+    cout<<endl;
 }
 
+// ----------- CASE 2 ------------
 void studentoPaieskaID() {
     string studentoIdPaieska;
     do {
         cout<<"Iveskite studento ID (arba 0, jei norite iseiti):";    
         cin>>studentoIdPaieska;
+        cout<<endl;
         if (studentoIdPaieska=="0") {
             cout<<"Iseinama"<<endl;
+            cout<<endl;
             break;
         }
-        auto ieskomasStudentas=find_if(studentai.begin(), studentai.end(), [&](const Student& studentas) {
+        auto ieskomasStudentas=find_if(studentai.begin(), studentai.end(), [&](const Student& studentas) { //find_if grazina vieta [&] leidzia naudoti kintamuosius is isores
         return studentas.studentID==studentoIdPaieska;
         });        
         if (ieskomasStudentas!=studentai.end()) {
@@ -176,26 +181,27 @@ void studentoPaieskaID() {
             cout<<"Pazymys (procentai): "<<static_cast<int>(ieskomasStudentas->studentGrade)<<" ("<<ieskomasStudentas->studentGradeProcent<<")"<<endl;
         }
         else cout<<"Ivestas mokinys nerastas."<<endl;
+        cout<<endl;
     } while (studentoIdPaieska!="0");
 }
 
 void outputFailas() {
     ofstream fout(FOUT);
+    int formatavimas=9;
     int studentoSk=1;
+    fout<<left<<setw(formatavimas-5)<<"Nr."<<left<<setw(formatavimas-5)<<"ID"<<left<<setw(formatavimas-1)<<"Taskai"<<left<<setw(formatavimas)<<"Pazymys"<<"Procentai"<<endl;
     for (auto i=studentai.begin(); i!=studentai.end(); i++) {
-        fout<<studentoSk<<") "<<i->studentID<<" "<<i->studentAtsakymai<<" "<<i->studentGrade<<" ("<<i->studentGradeProcent<<"%)"<<endl;
+        fout<<studentoSk<<left<<setw(formatavimas-6)<<")"<<left<<setw(formatavimas-5)<<i->studentID<<i->studentTaskai<<left<<setw(formatavimas)<<"/40"<<left<<setw(formatavimas)<<i->studentGrade<<i->studentGradeProcent<<"%"<<endl;
         studentoSk++;
     }
     fout.close();
 }
 
-void klausimuStatistika() {
+// ----------- CASE 3 ------------
+void klausimuStatistika(int atsakytiKlausimai[20][3]) {
     int formatavimas=11;
     int klausimuSk=20;
-    int klausimoIndex=klausimuSk-1;
-    int atsakytiKlausimai[20][3]={0};
-    cout<<left<<setw(formatavimas)<<"Klausimas"<<left<<setw(formatavimas)<<"Teisingai"<<left<<setw(formatavimas+2)<<"Neteisingai"<<"Neatsake"<<endl;
-    for (int j=1; j<=klausimuSk; j++ ) { //gal reikia klausimuSk+1 ????????
+    for (int j=1; j<=klausimuSk; j++ ) {
         for (auto i=studentai.begin(); i!=studentai.end(); i++) {
             if (i->studentAtsakymai[j-1]==teisingi_ats[j-1]) {
                 atsakytiKlausimai[j-1][0]++;
@@ -206,19 +212,45 @@ void klausimuStatistika() {
             else {
                 atsakytiKlausimai[j-1][1]++;
             }
+
         }
-        cout<<left<<setw(formatavimas)<<j<<left<<setw(formatavimas)<<atsakytiKlausimai[j-1][0]<<left<<setw(formatavimas)<<atsakytiKlausimai[j-1][1]<<atsakytiKlausimai[j-1][2]<<endl;
     }
-    cout<<endl;
 }
 
-void sunkiausiasKlausimas() {
+void klausimuStatistikaCout(int atsakytiKlausimai[20][3]) {
+    int formatavimas=11;
+    int klausimuSk=20;
+    cout<<left<<setw(formatavimas-6)<<"Nr."<<left<<setw(formatavimas)<<"Teisingai"<<left<<setw(formatavimas+2)<<"Neteisingai"<<"Neatsake"<<endl;
+    for (int j=1; j<=klausimuSk; j++) {
+    cout<<left<<setw(formatavimas-6)<<j<<left<<setw(formatavimas)<<atsakytiKlausimai[j-1][0]<<left<<setw(formatavimas+2)<<atsakytiKlausimai[j-1][1]<<atsakytiKlausimai[j-1][2]<<endl;
+    }
+}
 
+// ----------- CASE 4 ------------
+void sunkiausiasKlausimas(int atsakytiKlausimai[20][3]) {
+    int klausimuSk=20;
+    int klausimoNr=1;
+    int sunkiausiasKl=atsakytiKlausimai[0][0];
+    for (int i=1; i<klausimuSk; i++) {
+        if (atsakytiKlausimai[i][0]<sunkiausiasKl) {
+            sunkiausiasKl=atsakytiKlausimai[i][0];
+        }
+    }
+    cout<<"Sunkiausias klausimas: ";
+    for (int i=0; i<klausimuSk; i++) {
+        if (atsakytiKlausimai[i][0]==sunkiausiasKl) {
+            cout<<i+1<<" ";
+        }
+    }
+    cout<<" ("<<sunkiausiasKl<<"/"<<studentai.size()<<" atsake teisingai.)"<<endl;
+    cout<<endl;
 }
 
 int main() {
     int pasirinkimas;
+    int klausimas[20][3]={0};
     inputFailas();
+    klausimuStatistika(klausimas);
     studentoPazymys();
     do {
         pasirinkimoMenu();
@@ -234,11 +266,11 @@ int main() {
                 break;
             }
             case 3: {
-                klausimuStatistika();
+                klausimuStatistikaCout(klausimas);
                 break;
             }
             case 4: {
-                sunkiausiasKlausimas();
+                sunkiausiasKlausimas(klausimas);
                 break;
             }
             case 5: {
